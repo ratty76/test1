6b5637565c296ce9c6e152b3cff9902c961fcac1
Andy Ratcliffe <andrew.ratcliffe@reedbusiness.com>
2019-02-07 09:17:53 +0000

    Add products

---
+++ Test1/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    public interface IProduct
+    {
+        int TestFunc();
+
+    }
+}
